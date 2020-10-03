9cedeb9984b6295322de878187c9c6905c73ea94
Leonardo <lmvillanueva@gylgroup.com>
2020-10-03 10:57:41 -0300

    primera version

---
+++ music/src/components/Letras.js
@@ -0,0 +1,10 @@
+import React,{Fragment} from 'react';
+const Letras = ({cancion}) => {
+    return (  
+        <Fragment>
+            <h4>{cancion}</h4>
+        </Fragment>
+    );
+}
+ 
+export default Letras;
